f357ac55f7c773e548faa665f65fa4a87861f246
xzj6795365 <15852939122@163.com>
2018-08-31 11:43:05 +0800

    Create argparse.py

---
+++ argparse.py
@@ -0,0 +1,13 @@
+import argparse
+
+def get_args():
+  """Parse command line."""
+  filename_list = lambda x: x.split(";")
+  parser = argparse.ArgumentParser()
+  parser.add_argument("--input", type=filename_list,
+                      help="paths to input libraries separated by semicolons",
+                      required=True)
+  parser.add_argument("--output", help="output deffile", required=True)
+  parser.add_argument("--target", help="name of the target", required=True)
+  args = parser.parse_args()
+  return args
